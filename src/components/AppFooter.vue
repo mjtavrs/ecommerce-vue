469cbce134cd6d370daba266c0a2c73ee215db5a
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-08 20:03:25 -0300

    feat: Finish the first version of the AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
-    <p>Vue E-commerce &copy; 2025</p>
+    <footer>
+        <p>
+            This project was created by Marcos Tavares and is a part of the Vue.js studies.
+        </p>
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    padding-block: 1.5rem;
+    text-align: center;
+    width: 100%;
+}
+</style>
