31a63c6982350086ded02ad71ed3390b51d6494a
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-17 10:51:36 -0300

    feat: Create the ProductCard component and import in the HomeView for testing
    Also create the global style for the buttons

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,88 @@
+<script setup>
+const props = defineProps({
+    product: Object
+})
+</script>
+
+<template>
+    <div class="product-card">
+        <div class="product-card-image-container">
+            <img :src="product.image" :alt="product.title">
+        </div>
+        <div class="product-card-metadata">
+            <div class="product-card-metadata-wrapper">
+                <h3 class="product-card-metadata-title">{{ product.title }}</h3>
+                <span class="product-card-metadata-category">{{ product.category }}</span>
+                <p class="product-card-metadata-price">US$ {{ (product.price.toFixed(2)) }}</p>
+            </div>
+            <button>Add to cart</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.product-card {
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+    overflow: hidden;
+}
+
+.product-card-image-container {
+    align-items: center;
+    background-color: white;
+    display: flex;
+    height: 12.5rem;
+    justify-content: center;
+    padding: 2rem;
+    overflow: hidden;
+    width: 100%;
+
+    img {
+        max-height: 100%;
+        max-width: 100%;
+        object-fit: contain;
+    }
+}
+
+.product-card-metadata {
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+}
+
+.product-card-metadata-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+
+.product-card-metadata-title {
+    color: var(--light-black);
+    display: block;
+    font-size: 1.25rem;
+    font-weight: 500;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 100%;
+}
+
+.product-card-metadata-category {
+    background-color: var(--beige);
+    border-radius: 5px;
+    font-size: 0.75rem;
+    font-weight: 500;
+    padding: 0.2rem 0.6rem;
+    text-transform: uppercase;
+    max-width: fit-content;
+}
+
+.product-card-metadata-price {
+    font-size: 1.15rem;
+    font-weight: 500;
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,20 +1,31 @@
 <script setup>
 import { useEcommerceStore } from '../store/ecommerce.js'
+import { onMounted } from 'vue'
+import ProductCard from '../components/ProductCard.vue'
 
 const store = useEcommerceStore()
+
+onMounted(() => store.loadProducts())
 </script>
 
 <template>
     <div class="container">
-        <h1>Test to load the products:</h1>
-        <button @click="store.loadProducts">Load products</button>
+        <h1>Home</h1>
+        
+        <div v-if="!store.loadedProducts.length">
+            <p>Loading products...</p>
+        </div>
 
-        <pre v-if="store.loadedProducts.length">
-            {{ store.loadedProducts }}
-        </pre>
-        <p v-else>No products loaded</p>
+        <div v-else class="products-list">
+            <ProductCard v-for="product in store.loadedProducts" :key="product.id" :product="product" />
+        </div>
     </div>
 </template>
 
 <style scoped>
+.products-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    gap: 1rem;
+}
 </style>
