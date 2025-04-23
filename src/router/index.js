53cd220cd65010483ea63f995c2334617e1b5e10
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-23 16:16:59 -0300

    feat: Include a 404 page and change the name of the ProductDetail page to ProdutDetailView

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../pages/HomeView.vue'
 import CartView from '../pages/CartView.vue'
 import CheckoutView from '../pages/CheckoutView.vue'
-import ProductDetail from '../pages/ProductDetail.vue'
+import ProductDetailView from '../pages/ProductDetailView.vue'
+import NotFoundView from '../pages/NotFoundView.vue'
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/cart', component: CartView },
     { path: '/checkout', component: CheckoutView },
-    { path: '/product/:id', component: ProductDetail, props: true }
+    { path: '/product/:id', component: ProductDetailView, props: true },
+    { path: '/:catchAll(.*)', component: NotFoundView }
 ]
 
 export const router = createRouter({
